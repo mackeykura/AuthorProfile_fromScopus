f11def2bcc8bafbac44a564f5ba0b9e1a7f14488
Makiko <okazaki@kura.kyoto-u.ac.jp>
2018-08-23 11:15:07 +0900

    document countを追加しました

--- ScopusAPI_AuthorWithAsjc.py
+++ ScopusAPI_AuthorWithAsjc.py
@@ -1,7 +1,8 @@
 #! Python3
 # -*- coding: utf-8 -*-
 # Scopus Retrieval API
-# 4. 全著者IDそれぞれに専門分野を割り当てる
+# 著者IDに専門分野を割り当てる
+# API Keyの上限に達したら自動的にとまるように修正、論文数データを取得するコードを追加　180823
 
 import pandas as pd
 import requests
@@ -22,30 +23,31 @@ api =  'https://api.elsevier.com/content/author/author_id/{sid}?apiKey={key}'
 #4-1. 著者IDから分野（ASJC, 27分野） codeを抽出（API）
 author_asjc27 = []
 for i, number in uniqueaIds.iterrows():
-    # 2500人前後でerrorが出る（けどプログラムは回り続ける）ので、2600人で終了させる
-    if i > 2600:
-        break
-    else:
+    # 2500人前後でerrorが出るので、人数が多いときは以下のif文を使ってください
+#    if i < 2600:
+#        continue
+#    else:
+
         # APIのURLを得る
         aid = number['Author ID']
         url = api.format(sid=aid, key=myAPIkey)
         print('Scopus Author ID'+ str(i) + ' : ' + str(aid) )
 
-        # 実際にAPIにリクエストを送信して結果を取得する（errorが結構よく出るので、でたIDには99をつけて、止まらないようにする）
-        try:
+        # 実際にAPIにリクエストを送信して結果を取得する（errorが結構よく出るので、errorが出るIDには99をつけて、止まらないようにする）
+	# API Keyの上限に達したらとまるように修正
+	try:
             req = urllib.request.Request(url)
             with urllib.request.urlopen(req) as response:
                 r = requests.get(url)
         except URLError as e:
-            asjc27 = '99'
-            author27 = [aid, asjc27]
+            author27 = [aid, 0, 0, 0]
             print(author27)
-            author_asjc27.append(author27) 
+            author_asjc27.append(author27)
             if hasattr(e, 'reason'):
-                print('We failed to reach a server.')
                 print('Reason: ', e.reason)
+                if e.reason == 'Too Many Requests':
+                    break
             elif hasattr(e, 'code'):
-                print('The server couldn\'t fulfill the request.')
                 print('Error code: ', e.code)
         else:
             text = r.text.encode('utf-8')
@@ -53,8 +55,16 @@ for i, number in uniqueaIds.iterrows():
         
             # 結果を出力 (Author id, subject-areas)
             #  Defined Author ID
-            author_id = data[0][1].text
-            author_id = author_id.replace('AUTHOR_ID:','')
+            author_id = data[0][1].text.replace('AUTHOR_ID:','')
+            
+            # 論文数が5本以下ならスキップ
+            try:
+                dc = int(data.find('.//document-count').text)
+            except:
+                dc = 0
+		asjc27 = 0
+            
+            author27 = [aid, author_id, dc, asjc27]
             
             #author-profileの下から、分野のコードcode(4桁のうち上位2桁=asjc27)とその頻度frequencyを全て抽出
             asjc_list = []
@@ -72,11 +82,11 @@ for i, number in uniqueaIds.iterrows():
     
             #4-2. 一番多い分野を特定, 著者の分野を決定（分野の数が5個なければ分野なし＝0とする）
             asjc27 = asjcsort.iat[0,0]
-            author27 = [author_id, asjc27]
+            author27 = [aid, author_id, dc, asjc27]
             author_asjc27.append(author27)
     
 #4-4. listを作成
-author_asjc27_df = pd.DataFrame(author_asjc27, columns=['Scopus Author ID', 'asjc27'])
+author_asjc27_df = pd.DataFrame(author_asjc27, columns=['Scopus Author ID', 'responce ID', 'document count', 'asjc27'])
 author_asjc27_df.to_csv('AuthorIDwithasjc27List.csv', index = False)
 
  
